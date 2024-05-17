e0affd3fcc34f2b5d1d62d0ba83477da12f3a9c6
Ethel Benavidez <e.benavidez.dev@gmail.com>
2024-05-18 01:17:38 +0800

    fix for missing info variable

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,7 +1,8 @@
 import StickyTitle from '../StickyTitle.jsx'
 
 // eslint-disable-next-line react/prop-types
-const About = ({ className }) => {
+const About = ({ className, data }) => {
+  const info = data
   return (
     <div id='About' className={`text-slate-400 leading-loose lg:text-justify lg:px-4 xl:px-4 ${className}`}>
       <StickyTitle title="About" className='lg:hidden' />
